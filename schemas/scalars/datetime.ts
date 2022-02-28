9635eea60598bdb06173f2496b20bd05711a7d9b
marshalWilliam <marshalwilliam500@gmail.com>
2022-02-28 12:17:45 +0800

    Added ISO date string validation
    Added a validation in datetime custom scalar to check is input is a valid ISO date string

--- schemas/scalars/datetime.ts
+++ schemas/scalars/datetime.ts
@@ -1,3 +1,4 @@
+import { UserInputError } from "apollo-server-core";
 import { GraphQLError, GraphQLScalarType, Kind } from "graphql";
 
 export const dateTimeScalar = new GraphQLScalarType({
@@ -13,6 +14,12 @@ export const dateTimeScalar = new GraphQLScalarType({
     if (ast.kind !== Kind.STRING) {
       throw new GraphQLError(
         `Query Error: Can only parse strings as datetime but got a: ${ast.kind}`
+      );
+    }
+
+    if (ast.value !== new Date(Date.parse(ast.value)).toISOString()) {
+      throw new UserInputError(
+        `BAD_USER_INPUT: Date: ${ast.value} is not a valid ISO date string`
       );
     }
 
