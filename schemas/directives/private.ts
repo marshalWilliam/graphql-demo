cf609e8108fe2d10c86cda0338808cb67456ba37
marshalWilliam <98588086+marshalWilliam@users.noreply.github.com>
2022-03-03 20:29:13 +0800

    Update #2 @private directives
    - throwing Invalid Token error when user is not found and ID is not found.

--- schemas/directives/private.ts
+++ schemas/directives/private.ts
@@ -1,4 +1,5 @@
 import { mapSchema, getDirective, MapperKind } from "@graphql-tools/utils";
+import { ApolloError } from "apollo-server-core";
 import { GraphQLSchema, defaultFieldResolver } from "graphql";
 import { verify } from "jsonwebtoken";
 import { checkID } from "../functions/get";
@@ -16,18 +17,16 @@ export function privateDirectiveTransformer(
       )?.[0];
       if (privateDirective) {
         const { resolve = defaultFieldResolver } = fieldConfig;
+        let result: unknown;
 
         fieldConfig.resolve = async function (source, args, context, info) {
           const userInfo: any = verify(context.bearer, "RANDOM_STRING");
 
-          if (userInfo) {
-            context.user = await checkID(userInfo.user_id);
-            if (context.user) {
-              context.isAuth = true;
-            }
+          if (userInfo && (context.user = await checkID(userInfo.user_id))) {
+            result = await resolve(source, args, context, info);
+          } else {
+            throw new ApolloError("Invalid Token", "INVALID_TOKEN");
           }
-
-          const result = await resolve(source, args, context, info);
 
           return result;
         };
