0397c65e06ca94487a374ca919cca7a597c64f64
marshalWilliam <marshalwilliam500@gmail.com>
2022-03-07 18:52:15 +0800

    Add Query.node

--- src/schemas/resolvers/resolver_query.ts
+++ src/schemas/resolvers/resolver_query.ts
@@ -1,29 +1,77 @@
-import {
-  getAccounts,
-  getProducts,
-  getAccount,
-  getProduct,
-} from "../functions/get";
+import { UserInputError } from "apollo-server-core";
+import { getAccount, getProduct } from "../functions/get";
+import { Product } from "../functions/producttypes";
+import { ID, User } from "../functions/usertypes";
 
 export const queries_resolver = {
   Query: {
-    accounts: async () => {
-      return getAccounts();
+    node: async (_: undefined, { id }: ID) => {
+      let result: any;
+      if (id.includes("product", 32)) {
+        result = await getProduct(id);
+        if (result) {
+          return {
+            __typename: "Product",
+            result,
+          };
+        } else {
+          throw new UserInputError("ID not found");
+        }
+      } else if (id.includes("account", 32)) {
+        result = await getAccount(id);
+        if (result) {
+          return {
+            __typename: "Account",
+            result,
+          };
+        } else {
+          throw new UserInputError("ID not found");
+        }
+      } else {
+        throw new UserInputError("ID not found");
+      }
     },
-    products: async () => {
-      return getProducts();
+  },
+
+  Account: {
+    id: ({ result }: User) => {
+      return result.id;
     },
-    account: async (_: never, args: any) => {
-      return getAccount(args.id);
+    firstname: ({ result }: User) => {
+      return result.firstname;
     },
-    product: async (_: never, args: any) => {
-      return getProduct(args.id);
+    lastname: ({ result }: User) => {
+      return result.lastname;
+    },
+    emailAddress: ({ result }: User) => {
+      return result.emailAddress;
+    },
+    createdAt: ({ result }: User) => {
+      return result.createdAt;
+    },
+    updatedAt: ({ result }: User) => {
+      return result.updatedAt;
     },
   },
 
   Product: {
-    owner: async (parent: any) => {
-      return getAccount(parent.owner);
+    id: ({ result }: Product) => {
+      return result.id;
+    },
+    name: ({ result }: Product) => {
+      return result.name;
+    },
+    description: ({ result }: Product) => {
+      return result.description;
+    },
+    owner: async ({ result }: Product) => {
+      return { result: await getAccount(result.owner) };
+    },
+    createdAt: ({ result }: Product) => {
+      return result.createdAt;
+    },
+    updatedAt: ({ result }: Product) => {
+      return result.updatedAt;
     },
   },
 };
